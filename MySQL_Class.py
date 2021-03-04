756cdb6e805d9a0faca35c104bfa5b6301aabad0
GERMAN BARRERA <juandanro200030@gmail.com>
2021-03-04 16:11:07 -0500

    Primera version

---
+++ MySQL_Class.py
@@ -0,0 +1,55 @@
+import mysql.connector
+import DBConfig
+
+class MySQL():
+    # ---------------------------------------------------------
+    def connect(self):
+
+        conn = mysql.connector.connect(**DBConfig.dbConfig)
+
+        # Crear Cursor
+        cursor = conn.cursor()
+
+        # Retornar Coneccion y Cursor
+        return conn, cursor
+    # ---------------------------------------------------------
+
+    # ---------------------------------------------------------
+    def close(self, cursor, conn):
+        # Cerrar el cursor
+        cursor.close()
+
+        # Cerrar coneccion con MySQL
+        conn.close()
+    # ---------------------------------------------------------
+
+    # ---------------------------------------------------------
+    def insertdata(self, alarma):
+
+        # Conectar con MySQL
+        conn, cursor = self.connect()
+
+        # Insertar los datos
+        cursor.execute("INSERT INTO parpadeos (id,alarma) VALUES ({},{})".format("NULL",alarma))
+
+        conn.commit()
+
+        # Cerrar el cursor y la coneccion
+        self.close(cursor,conn)
+    # ---------------------------------------------------------
+
+    # ---------------------------------------------------------
+    def consultData(self):
+
+        # Conectar con MySQL
+        conn, cursor = self.connect()
+
+        # Consultar los datos
+        cursor.execute("select * from parpadeos order by id desc limit 1;")
+        respuesta = cursor.fetchall()
+
+        # Cerrar el cursor y la coneccion
+        self.close(cursor,conn)
+
+        return respuesta
+    # ---------------------------------------------------------
